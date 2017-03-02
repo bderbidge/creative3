6df281dfc1f73ff58305251f1c8a37e591dfe678
Tanner <tannerdavisr@gmail.com>
2017-03-01 22:33:01 -0700

    Angular basics
    Middle finger

--- js/Controller.js
+++ js/Controller.js
@@ -1,19 +1,62 @@
-angular.module('app', [])
-    .controller('mainCtrl', mainCtrl)
-    .directive('calculator', calculatorDirective);
-    
-    
-function mainCtrl ($scope) {
-    $scope.users = [];
-  
-    $scope.addNew = function (user) {
-        $scope.users.push({
-            name: user.name,
-            avatarUrl: user.url,
-            email: user.email
-        });
-        user.name = '';
-        user.url = '';
-        user.email = '';
-    };
+var myApp = angular.module('myApp', []);
+
+ myApp.controller('mainCtrl',function ($scope) {  
+
+
+ $scope.txt = '';
+ $scope.numbers = [];
+ $scope.operators = [];
+
+ $scope.calculate = function() {
+    var answer = 0;
+
+    while($scope.operators.length > 0) {
+      var right = $scope.numbers.pop();
+      if(isNaN(right)){
+        right = 0;
+      }
+      var left = $scope.numbers.pop();
+      if(isNaN(left)){
+        left = 0;
+      }
+      var op = $scope.operators.pop();
+        
+      answer += calc(left, right, op);
+
+    }
+
+    $scope.result = answer;
+    $scope.numbers = [];
+    $scope.operations = [];
+    $scope.txt = '';
+};
+
+$scope.addNumber = function(num) { 
+    $scope.txt += num; 
+    $scope.numbers.push(parseInt(num));
+};
+
+$scope.addOperator = function(operator){
+   $scope.txt += operator;
+   $scope.operators.push(operator);
 }
+
+ function calc(lhs,rhs,op){
+  switch(op){
+    case '+':
+      return lhs + rhs;
+    case '-':
+      return lhs - rhs;
+    case '*':
+      return lhs * rhs;      
+    case '/':
+      return lhs / rhs;
+    case '%':
+      return lhs % rhs;
+  }
+  return 0;
+}
+
+
+
+});
